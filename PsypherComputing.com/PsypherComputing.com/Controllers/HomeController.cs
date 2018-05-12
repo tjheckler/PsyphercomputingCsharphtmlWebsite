7ee4ece259e981bfae4b441c018b617424f06238
Timothy Heckler <timothy_heckler@outlook.com>
2018-05-11 21:28:35 -0500

    Personal Website

---
+++ PsypherComputing.com/PsypherComputing.com/Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PsypherComputing.com.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Contact Me";
+
+            return View();
+        }
+
+        public ActionResult Information()
+        {
+            return View();
+        }
+
+        public ActionResult Gallery()
+        {
+            return View();
+        }
+    }
+
+}
---
+++ PsypherComputing.com/PsypherComputing.com/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="PsypherComputing.com.MvcApplication" Language="C#" %>
